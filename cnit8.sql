505e4548a9546c2c2d9ef72c41d200c95baed8d6
rongqiudeng <375170667@qq.com>
2018-05-28 18:50:05 +0800

    同步数据表

--- cnit8.sql
+++ cnit8.sql
@@ -10,7 +10,7 @@ Target Server Type    : MariaDB
 Target Server Version : 100213
 File Encoding         : 65001
 
-Date: 2018-05-23 17:52:38
+Date: 2018-05-28 18:49:32
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -42,7 +42,7 @@ CREATE TABLE `cn_admin` (
 -- ----------------------------
 -- Records of cn_admin
 -- ----------------------------
-INSERT INTO `cn_admin` VALUES ('5', 'admin', '2d2a21b2754a45c815efa9d2e1be8459', '1', '18729309523', 'rongqiu', 'xian', 'sss', '1', '2048', '1', 'ssss', '2018-05-23 17:34:40', '127.0.0.1', '2018-05-23 17:34:40', '2018-05-23 17:34:40');
+INSERT INTO `cn_admin` VALUES ('5', 'admin', '2d2a21b2754a45c815efa9d2e1be8459', '1', '18729309523', 'rongqiu', 'xian', 'sss', '1', '2048', '1', 'ssss', '2018-05-28 17:11:04', '127.0.0.1', '2018-05-28 17:11:04', '2018-05-28 17:11:04');
 
 -- ----------------------------
 -- Table structure for cn_article
@@ -55,20 +55,26 @@ CREATE TABLE `cn_article` (
   `description` varchar(450) NOT NULL,
   `author` varchar(20) NOT NULL,
   `image` varchar(500) NOT NULL,
-  `is_top` tinyint(2) NOT NULL,
-  `add_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
+  `is_top` tinyint(1) NOT NULL,
+  `add_time` datetime DEFAULT NULL,
   `container` mediumtext NOT NULL,
-  `create_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
-  `update_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
+  `is_delete` tinyint(1) DEFAULT 0,
+  `create_time` datetime DEFAULT '0000-00-00 00:00:00',
+  `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of cn_article
 -- ----------------------------
-INSERT INTO `cn_article` VALUES ('2', '0', '测试', '彩色', 'asd', '/upload/images/20180522/cf028dd868428827641ff19c86c9c019.png', '0', '2018-05-23 17:49:16', '<p>asdasdasda你打没打adadad那什么大事啊啊所大所<img src=\"/upload/ueditImage/20180522/1526970662637664.jpg\" title=\"1526970662637664.jpg\" alt=\"pic.jpg\"/>adada</p>', '2018-05-23 17:49:16', '2018-05-23 17:49:16');
-INSERT INTO `cn_article` VALUES ('3', '0', '测试', '彩色', 'admin', '/upload/images/20180522/0016005d9ee08dc9f02c25829d9a87a7.png', '0', '2018-05-22 14:33:53', '<p>爱仕达所大<img src=\"/upload/ueditImage/20180522/1526970759244993.jpg\" title=\"1526970759244993.jpg\" alt=\"tp.jpg\"/></p>', '2018-05-22 14:33:53', '2018-05-22 14:33:53');
-INSERT INTO `cn_article` VALUES ('4', '0', 'adasd', 'aadsa', 'dasda', '/upload/images/20180523/acb5a19eddf467225aaa5ff9b598c411.png', '0', '2018-05-23 17:49:58', '<p>adasdasda<br/></p>', '2018-05-23 17:49:58', '2018-05-23 17:49:58');
+INSERT INTO `cn_article` VALUES ('2', '0', '测试', '彩色', 'asd', '/upload/images/20180522/cf028dd868428827641ff19c86c9c019.png', '0', '2018-05-28 16:34:27', '<p>asdasdasda你打没打adadad那什么大事啊啊所大所<img src=\"/upload/ueditImage/20180522/1526970662637664.jpg\" title=\"1526970662637664.jpg\" alt=\"pic.jpg\"/>adadagg</p>', '0', '2018-05-25 10:40:30', '2018-05-28 16:34:27');
+INSERT INTO `cn_article` VALUES ('3', '0', '测试', '彩色', 'admin', '/upload/images/20180522/0016005d9ee08dc9f02c25829d9a87a7.png', '0', '2018-05-23 18:04:18', '<p>爱仕达所大<img src=\"/upload/ueditImage/20180522/1526970759244993.jpg\" title=\"1526970759244993.jpg\" alt=\"tp.jpg\"/></p>', '0', '2018-05-23 18:04:18', '2018-05-23 18:04:18');
+INSERT INTO `cn_article` VALUES ('4', '0', 'adasd', 'aadsa', 'dasda', '/upload/images/20180523/acb5a19eddf467225aaa5ff9b598c411.png', '0', '2018-05-23 17:49:58', '<p>adasdasda<br/></p>', '0', '2018-05-23 17:49:58', '2018-05-23 17:49:58');
+INSERT INTO `cn_article` VALUES ('5', '11', 'mysql', 'mysql配置了数据库连接信息后，我们就可以直接使用数据库运行原生SQL操作了，支持query（查询操作）和execute（写入操作）方法，并且支持参数绑定。', '邓荣秋', '/upload/images/20180528/bc0deeae90fee229d18552ccea28381c.jpg', '1', '2018-05-28 17:12:09', '<p>mysql是很不错的<br/></p>', '0', '2018-05-28 17:12:09', '2018-05-28 17:14:25');
+INSERT INTO `cn_article` VALUES ('6', '10', 'PHP', 'php是最好的语言', 'admin', '/upload/images/20180528/91933a22e61ad1cd1851e08211c6150e.jpg', '0', '2018-05-28 17:28:25', '<p>php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言</p>', '0', '2018-05-28 17:28:25', null);
+INSERT INTO `cn_article` VALUES ('7', '10', 'php是最好的语言', 'php是最好的语言', 'admin', '/upload/images/20180528/fe57ba294bd7b234ba0633f8bae2afe7.png', '1', '2018-05-28 17:29:44', '<p>php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言php是最好的语言</p>', '0', '2018-05-28 17:29:44', null);
+INSERT INTO `cn_article` VALUES ('8', '10', 'html测试', 'html测试', 'admin', '/upload/images/20180528/74889c25d69b120a32efcecdf6e5b3b7.jpg', '0', '2018-05-28 18:38:51', '<p>html测试html测试html测试html测试html测试</p>', '0', '2018-05-28 18:38:51', '2018-05-28 18:40:36');
+INSERT INTO `cn_article` VALUES ('9', '10', 'html测试', 'html测试', 'admin', '/upload/images/20180528/b132eedf0f7eb58593ee82e1fd8e97ce.jpg', '0', '2018-05-28 18:44:15', '<p>asdasdasdasdasasdasdas<img src=\"/upload/ueditImage/20180528/1527504234119804.jpg\" title=\"1527504234119804.jpg\" alt=\"pic.jpg\"/></p>', '0', '2018-05-28 18:44:15', null);
 
 -- ----------------------------
 -- Table structure for cn_article_type
@@ -76,13 +82,22 @@ INSERT INTO `cn_article` VALUES ('4', '0', 'adasd', 'aadsa', 'dasda', '/upload/i
 DROP TABLE IF EXISTS `cn_article_type`;
 CREATE TABLE `cn_article_type` (
   `id` int(3) NOT NULL AUTO_INCREMENT,
-  `type_name` varchar(255) DEFAULT NULL,
+  `type_name` varchar(255) NOT NULL DEFAULT '',
+  `create_user` varchar(30) NOT NULL DEFAULT '',
+  `is_delete` tinyint(1) NOT NULL DEFAULT 0,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of cn_article_type
 -- ----------------------------
+INSERT INTO `cn_article_type` VALUES ('10', 'php', 'admin', '0', '2018-05-28 16:27:58', null);
+INSERT INTO `cn_article_type` VALUES ('11', 'mysqli', 'admin', '0', '2018-05-28 16:30:13', '2018-05-28 18:00:45');
+INSERT INTO `cn_article_type` VALUES ('12', 'javascript', 'admin', '0', '2018-05-28 16:30:31', '2018-05-28 18:39:45');
+INSERT INTO `cn_article_type` VALUES ('13', 'java', 'admin', '0', '2018-05-28 17:59:23', null);
+INSERT INTO `cn_article_type` VALUES ('14', 'html', 'admin', '0', '2018-05-28 18:38:17', null);
 
 -- ----------------------------
 -- Table structure for it_admin
